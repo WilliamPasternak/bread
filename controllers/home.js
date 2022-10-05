53edf336966b7c37a862bf2a8b55ece048e8a7d9
William Pasternak <31456405+WilliamPasternak@users.noreply.github.com>
2022-10-05 05:13:14 -0400

    Website Copy, Logo Change, Color Change

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,45 @@
+const Post = require("../models/Post");
+
+/*
 module.exports = {
   getIndex: (req, res) => {
     res.render("index.ejs",{ title: 'bread | Pay Transparency for the Hospitality Industry' });
   },
 };
+*/
+
+/*
+module.exports = {
+  getIndex: (req, res) => {
+  try {
+    res.render("index.ejs",{ title: 'bread | Pay Transparency for the Hospitality Industry' });
+  } catch (err) {
+    console.log(err);
+  }
+}}
+
+*/
+
+
+module.exports = {
+  getIndex: async (req, res) => {
+  try {
+    const posts = await Post.find().lean();
+    res.render("index.ejs",{ title: 'bread | Pay Transparency for the Hospitality Industry', posts: posts });
+  } catch (err) {
+    console.log(err);
+  }
+}}
+
+/*
+getFeed: async (req, res) => {
+  try {
+    const posts = await Post.find().lean();
+    const post = await Post.findById(req.params.id);
+    res.render("feed.ejs", { posts: posts, post: post,title: 'bread | Recent Salaries' });
+  } catch (err) {
+    console.log(err);
+  }
+},
+
+*/
